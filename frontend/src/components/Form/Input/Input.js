69abc4941b811b38532a1c0a51777c6e5bd7f3f2
rabelorp <rabelo_rp@hotmail.com>
2023-03-03 10:52:25 -0300

    fix: erros

--- frontend/src/components/Form/Input/Input.js
+++ frontend/src/components/Form/Input/Input.js
@@ -1,22 +1,33 @@
-import { Input as UI5Input, ValueState } from '@ui5/webcomponents-react';
-import { spacing } from '@ui5/webcomponents-react-base';
-import React from 'react';
-import FieldBase from '../FieldBase/FieldBase';
+import { Input as UI5Input, ValueState } from "@ui5/webcomponents-react";
+import { spacing } from "@ui5/webcomponents-react-base";
+import React from "react";
+import FieldBase from "../FieldBase/FieldBase";
 
-const Input = ({ field, form: { touched, errors }, labelText, style, ...props }) => {
+const Input = ({
+  field,
+  form: { touched, errors },
+  labelText,
+  style,
+  ...props
+}) => {
   const errorMsg = touched[field.name] && errors[field.name];
   const errorState = errorMsg ? ValueState.Error : ValueState.None;
 
   const innerStyle = {
     ...style,
     ...spacing.sapUiTinyMarginBottom,
-    width: '100%'
+    width: "100%",
   };
 
   const { onChange, onBlur, ...formikProps } = field;
 
   return (
-    <FieldBase labelText={labelText} required={props.required} for={props.for} showColon={props.showColon}>
+    <FieldBase
+      labelText={labelText}
+      required={props.required}
+      htmlFor={props.for}
+      // showColon={props.showColon}
+    >
       <UI5Input
         data-testid="input-wrapper"
         valueState={errorState}
--- frontend/src/components/Form/TextArea/TextArea.js
+++ frontend/src/components/Form/TextArea/TextArea.js
@@ -1,17 +1,24 @@
-import { TextArea as UI5TextArea, ValueState } from '@ui5/webcomponents-react';
-import { spacing } from '@ui5/webcomponents-react-base';
-import React from 'react';
+import { TextArea as UI5TextArea, ValueState } from "@ui5/webcomponents-react";
+import { spacing } from "@ui5/webcomponents-react-base";
+import React from "react";
 
-import FieldBase from '../FieldBase/FieldBase';
+import FieldBase from "../FieldBase/FieldBase";
 
-const TextArea = ({ field, form: { touched, errors }, labelText, rows, style, ...props }) => {
+const TextArea = ({
+  field,
+  form: { touched, errors },
+  labelText,
+  rows,
+  style,
+  ...props
+}) => {
   const errorMsg = touched[field.name] && errors[field.name];
   const errorState = errorMsg ? ValueState.Error : ValueState.None;
 
   const { onChange, onBlur, ...formikProps } = field;
 
   return (
-    <FieldBase labelText={labelText} required={props.required} for={props.for} showColon={props.showColon}>
+    <FieldBase labelText={labelText} required={props.required} for={props.for}>
       <UI5TextArea
         data-testid="textArea-wrapper"
         valueState={errorState}
